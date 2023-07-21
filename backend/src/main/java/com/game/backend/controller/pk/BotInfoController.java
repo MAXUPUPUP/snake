7df52ed0af2a870068bec990e8a7e6b60ddef183
仕啸 桂 <569977507@qq.com>
2023-07-21 23:43:25 +0800

    项目骨架搭建好乐

--- backend/src/main/java/com/game/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/game/backend/controller/pk/BotInfoController.java
@@ -13,16 +13,10 @@ import java.util.Map;
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo(){
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo(){
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "tiger");
         bot1.put("rating", "7777");
-        Map<String, String> bot2 = new HashMap<>();
-        bot2.put("name", "kugga");
-        bot2.put("rating", "6666");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
