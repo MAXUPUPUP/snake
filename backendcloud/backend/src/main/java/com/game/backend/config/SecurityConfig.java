aff7f6a0186f95a126a7d8120d231b9493a277e9
MAXUPUPUP <569977507@qq.com>
2026-05-15 22:16:50 +0900

    deleted the repeated part-consumer

--- backendcloud/backend/src/main/java/com/game/backend/config/SecurityConfig.java
+++ backendcloud/backend/src/main/java/com/game/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/user/account/token/", "/user/account/register/").permitAll()
+                .antMatchers("/user/account/token", "/user/account/register/").permitAll()
                 .antMatchers("/pk/start/game/", "/pk/receive/bot/move/").hasIpAddress("127.0.0.1")
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated();
